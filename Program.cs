d385d96927a816908c31a2b72178ff57d359ea49
Sanchit Agarwal <sanchita@quicinc.com>
2021-07-20 15:04:56 -0700

    links changed

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace autoupdateTrial
     {
         static void Main(string[] args)
         {
-            AutoUpdater.Start("https://github.com/sanchit1591/autoupdateTrial/blob/master/XMLFile1.xml");
+            AutoUpdater.Start("https://github.com/sanchit1591/autoupdateTrial/blob/master/updater.xml");
             Console.WriteLine("Hello World!");
         }
     }
